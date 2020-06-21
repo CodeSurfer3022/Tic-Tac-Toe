5030d8e3a3f77820e80e53ad11ad888cfb9c3223
Chandra Prakash <pr.chandraprakash@gmail.com>
2020-06-21 17:04:47 +0530

    Marked cells can't be modified

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,10 @@
 function placeMark() {
     console.log(this);
+    mark = this.firstChild.textContent;
+    if(mark) return;
     this.firstChild.textContent = game.turn.mark;
     game.turn = game.turn === playerOne ? playerTwo : playerOne;
+    heading.textContent = heading.textContent === 'Player 1' ? 'Player 2' : "Player 1";
     console.log(game.turn.mark);
 }
 
@@ -15,6 +18,7 @@ function renderBoard(board) {
         }
     );
 }
+
 //////////////////////////////
 const player = (mark) => {
     return {mark};
@@ -42,3 +46,6 @@ renderBoard(gameBoard.board);
 
 const cells = document.querySelectorAll('#board div');
 cells.forEach(cell => cell.addEventListener('click', placeMark));
+
+const heading = document.querySelector('h1');
+console.log(heading)
